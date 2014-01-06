748fbb11dab1c797ac4a8a2daa979be2f4c41c02
anthonyBBH <anthony.terruso@bartleboglehegarty.com>
2014-01-06 09:25:13 -0500

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+function activationCode(){
+        var thisYear = document.getElementById("Field22").value;
+        // if (thisYear){
+        //         alert("Correct");
+        // } else {
+        //         alert("Not Current Year");
+        // };
+
+        console.log(thisYear);
+};
